2a9f97d08ca51f2156fbbf661b852a0d49601dc1
Android Studio <devandroid@ort>
2024-07-03 10:41:41 -0300

    terminar el athentication helper

---
+++ src/helpers/authetication-helper.js
@@ -0,0 +1,12 @@
+import jwt from 'jsonwebtoken';
+
+export default class AutheticationHelper
+{
+    authenticationToke = async (token) => {
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+}
--- src/repositories/users-repositories.js
+++ src/repositories/users-repositories.js
@@ -1,9 +1,5 @@
 import DataBaseHelper from '../helpers/db-helper.js';
 import jwt from 'jsonwebtoken';
-import DBConfig from './../configs/dbConfig.js';
-import pkg from 'pg';
-const {Client} = pkg;
-
 
 export default class UsersRepository
 {
@@ -11,10 +7,8 @@ export default class UsersRepository
     {
         let returnArray = null;
         const KEY = 'claveToken';
-        const client = new Client(DBConfig);
         try
         {
-            await client.connect();
             const login =
             {
                 username: entity.username,
@@ -26,8 +20,7 @@ export default class UsersRepository
             };
             const sql = `select * From users Where username= $1 And password= $2`;
             const values = [entity.username, entity.password];
-            const consulta = await client.query(sql, values);
-            await client.end();
+            const consulta = DataBaseHelper.requestValues(sql, values);
             if(consulta != null)
             {
                 const token = jwt.sign(login, KEY, options);
@@ -37,7 +30,6 @@ export default class UsersRepository
                     message: '',
                     token: token
                 }
-                console.log(result);
                 returnArray = result;
             }
         }
@@ -51,8 +43,6 @@ export default class UsersRepository
     RegisterAsync = async (entity) =>
     {
         let returnArray = null;
-        const client = new Client(DBConfig);
-        await client.connect();
         const sql = `Insert into Users(first_name, last_name, username, password) Values ($1,$2,$3,$4)`;
         const values = [entity.first_name, entity.last_name, entity.username, entity.password];
         returnArray = DataBaseHelper.requestCount(sql, values);
